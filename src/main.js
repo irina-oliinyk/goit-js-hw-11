9a9918ecaa832f7f14511fcc2c04bf1b1f7c843b
Iryna Oliinyk <arairishka2@gmail.com>
2024-07-07 01:59:58 +0200

    update 3.0

--- src/main.js
+++ src/main.js
@@ -2,54 +2,59 @@
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
-import Group from './img/error.svg'
+import icon from './img/error.svg'
+
+// Описаний у документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 import { fetchphoto } from "./js/pixabay-api";
-// import { createMarcap } from "./js/render-function";
+import { createMarcap } from "./js/render-function";
 
 
 
 let inpurUser = document.querySelector('#name-input');
 const formEl = document.querySelector('.form');
 const containerCard = document.querySelector('.card-container');
+const list = document.querySelector('.gallery-conteiner');
+const loader = document.querySelector('.loader');
 
 
-
-function createMarcap(arr) {
-  containerCard.innerHTML = '';
-  return arr
-    .map(
-      ({ largeImageURL, previewURL, likes, views, comments, downloads }) =>
-        `<ul class="gallery">
-          <li class="gallery-item">
-           <a class="gallery-link" href="${largeImageURL}">
-            <img src="${previewURL}" alt="" width="300" height="140"/> 
-            </a>
-                <ul class="galery-item-description">
-                    <li>
-                        <p class="paragraf">Likes</p>
-                        <p class="value">${likes}</p>
-                    </li>
-                    <li>
-                        <p class="paragraf">Views</p>
-                        <p class="value">${views}</p>
-                    </li>
-                    <li>
-                        <p class="paragraf">Comments</p>
-                        <p class="value">${comments}</p>
-                    </li>
-                    <li>
-                        <p class="paragraf">Downloads</p>
-                        <p class="value">${downloads}</p>
-                    </li>
-                </ul>
-           </li>
-        </ul>
-      `
-    )
-    .join('');
-};
+// function createMarcap(arr) {
+//   clearGallery();
+  
+//   return arr
+//     .map(
+//       ({ largeImageURL, previewURL, likes, views, comments, downloads }) =>
+//         `<li class="gallery gallery-item">
+//            <a class="gallery-link" href="${largeImageURL}">
+//             <img src="${previewURL}" alt="" width="300" height="140"/> 
+//             </a>
+//                 <ul class="galery-item-description">
+//                     <li>
+//                         <p class="paragraf">Likes</p>
+//                         <p class="value">${likes}</p>
+//                     </li>
+//                     <li>
+//                         <p class="paragraf">Views</p>
+//                         <p class="value">${views}</p>
+//                     </li>
+//                     <li>
+//                         <p class="paragraf">Comments</p>
+//                         <p class="value">${comments}</p>
+//                     </li>
+//                     <li>
+//                         <p class="paragraf">Downloads</p>
+//                         <p class="value">${downloads}</p>
+//                     </li>
+//                 </ul>
+//            </li>   
+//       `
+//     )
+//     .join('');
+// };
 
 
 
@@ -58,28 +63,40 @@ formEl.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  clearGallery();
- 
+  // clearGallery();
+  loader.classList.remove('visually-hidden');
    const form = event.currentTarget; // посилання на елемент форми
    const queryValue = form.elements.query.value.toLowerCase(); // значення, яке написав користувач
-
-
-   if (queryValue === '') {
-     alert('Error');
+  if (queryValue === '') {
+     iziToast.show({
+      backgroundColor: '#ef4040',
+      close: false,
+      closeOnClick: true,
+      progressBarColor: 'white',
+      title: 'Error',
+      titleColor: 'white',
+      position: 'topRight',
+      messageColor: 'white',
+      messageSize: '16px',
+      message: 'Sorry, there are no images matching your search query. Please try again!',
+      icon: 'icon-error.svg',
+      iconUrl: icon
+        });
    } else {
    console.log(queryValue);
-   inpurUser.value = '';
-  }
-  let valueCategori = queryValue.toLowerCase();
+    inpurUser.value = '';
+    let valueCategori = queryValue.toLowerCase();
   fetchphoto(valueCategori)
     .then(data => {
       let hitsArray = data.hits;
       console.log(hitsArray);
       displayImages(hitsArray);
-      containerCard.insertAdjacentHTML('afterbegin', createMarcap(hitsArray));
-      // clearGallery()
+      loader.classList.add('visually-hidden');
+      
     })// запускаємо функцію, яка відмалює наші карточки
   .catch(onFetchError); // якщо робиться запит на неіснуючого покемона (404) - викликається ця фукнція для обробки помилки
+  }
+  
 }
 
 
@@ -93,39 +110,41 @@ function onFetchError(error) {
           progressBarColor: 'white',
           title: 'Error',
           titleColor: 'white',
-          iconUrl: Group,
           position: 'topRight',
           icon: 'icon-error.svg',
           messageColor: 'white',
           messageSize: '16px',
-          message: 'Sorry, there are no images matching your search query. Please try again!'
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          iconUrl: icon
         });
 };
 
 
 function clearGallery() {
-    containerCard.innerHTML = '';
+  list.innerHTML = '';
 }
 
 function displayImages(hitsArray) {
   clearGallery();
-
+ 
   if (hitsArray.length === 0) {
-   iziToast.show({
+    loader.classList.add('visually-hidden');
+    return iziToast.show({
           backgroundColor: '#ef4040',
           close: false,
           closeOnClick: true,
           progressBarColor: 'white',
           title: 'Error',
           titleColor: 'white',
-          iconUrl: Group,
           position: 'topRight',
           icon: 'icon-error.svg',
           messageColor: 'white',
           messageSize: '16px',
-          message: 'Sorry, there are no images matching your search query. Please try again!'
-        });
-  }else if (hitsArray.length < 40) {
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          iconUrl: icon
+    });
+    
+  } else if (hitsArray.length < 40) {
     iziToast.show({
           backgroundColor: '#ef4040',
           close: false,
@@ -133,13 +152,22 @@ function displayImages(hitsArray) {
           progressBarColor: 'white',
           title: 'Error',
           titleColor: 'white',
-          iconUrl: Group,
+          iconUrl: icon,
           position: 'topRight',
           icon: 'icon-error.svg',
           messageColor: 'white',
           messageSize: '16px',
           message: 'Sorry, das ist alles, was wir haben!'
-        });
+    });
+    list.insertAdjacentHTML('afterbegin', createMarcap(hitsArray));
+  } else {
+    list.insertAdjacentHTML('afterbegin', createMarcap(hitsArray));
   }
 }
 
+
+var lightbox = new SimpleLightbox('.gallery-item a', { 'captionsData': 'alt', 'captionDelay': 250, 'preloading': true });
+ 
+console.log(lightbox);
+
+lightbox.refresh();
